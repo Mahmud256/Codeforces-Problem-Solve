092b4f5c4288c8a5510e8c2790400a9ddc9cad22
Mahmudul Hasan Sarkar <71069222+Mahmud256@users.noreply.github.com>
2022-03-02 08:59:50 +0600

    Create 281A - Word Capitalization.cpp

---
+++ 281A - Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int i;
+    cin>>s;
+
+    s[0]=towupper(s[0]);
+
+    cout<<s;
+  
+    return 0;
+}
+ 
