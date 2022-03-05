d10c16b88499eedd25ef0c7f4f49bda6f3b3dbbc
Mahmudul Hasan Sarkar <71069222+Mahmud256@users.noreply.github.com>
2022-03-05 11:26:54 +0600

    Create 492B - Vanya and Lanterns.cpp

---
+++ 492B - Vanya and Lanterns.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,l,i,m=0;
+    cin>>n>>l;
+    int arr[n];
+
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    sort(arr,arr+n);
+
+    for(i=0; i<n-1; i++)
+    {
+        if(arr[i+1]-arr[i]>m)
+        {
+            m = arr[i+1]-arr[i];
+        }
+    }
+
+    double x = arr[0];
+    double y = (double)m/2;
+    double ans = max(x,y);
+    double z = l - arr[n-1];
+    ans = max(z,ans);
+
+    cout<<fixed<<setprecision(10)<<ans;
+
+    return 0;
+}
