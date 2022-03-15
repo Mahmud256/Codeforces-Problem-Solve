98d4db8c3c2f1dc8a7e6e7e14c36a16c013e5421
Mahmudul Hasan Sarkar <71069222+Mahmud256@users.noreply.github.com>
2022-03-15 10:23:48 +0600

    Create 1549A - Gregor and Cryptography.cpp

---
+++ 1549A - Gregor and Cryptography.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,i,n;
+
+    cin>>t;
+
+    for(i=0; i<t; i++)
+    {
+        cin>>n;
+        cout<<2<<" "<<n-1<<endl;
+    }
+
+    return 0;
+}
