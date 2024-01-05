9b67092adedf6e11dbd593ff7731cd16dd2dda0b
Mahmud Hasan <mahmud474849@gmail.com>
2024-01-06 00:29:55 +0600

    1791A

---
+++ python/1791A - Codeforces Checking.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n):
+    c = input()
+    if c=='c' or c=='o' or c=='d' or c=='e' or c=='f' or c=='r' or c=='s':
+        print("YES")
+    else:
+        print("NO")
