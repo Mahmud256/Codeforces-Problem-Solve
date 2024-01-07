efdfa1a5d58cbeca64d3c67678348809174b3f37
Mahmud Hasan <mahmud474849@gmail.com>
2024-01-07 10:27:04 +0600

    Wallet

---
+++ C++/A. Wallet Exchange.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, a, b;
+    cin >> n;
+
+    while (n--)
+    {
+        cin >> a >> b;
+
+        if ((a + b) % 2 == 0)
+        {
+            cout << "Bob" << endl;
+        }
+        else
+        {
+            cout << "Alice" << endl;
+        }
+    }
+
+    return 0;
+}
