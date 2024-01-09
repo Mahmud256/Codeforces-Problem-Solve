0bb1ca809111fdc9f57c62c411ebc994b4a94745
Mahmud Hasan <mahmud474849@gmail.com>
2024-01-10 00:26:33 +0600

    339B

---
+++ code practice/sd.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int s = 0, d = 0, f = 1;
+
+    while ( !v.empty() ) {
+        if ( f1 == 1 ) {
+            if ( v[0] > v.back() ) {
+                s += v[0];
+                v.erase( v.begin() );
+            }
+            else {
+                s += v.back();
+                v.pop_back();
+            }
+ 
+            f1 = 2;
+        }
+ 
+        else {
+            if ( v[0] > v.back() ) {
+                d += v[0];
+                v.erase( v.begin() );
+            }
+            else {
+                d += v.back();
+                v.pop_back();
+            }
+ 
+            f1 = 1;
+        }
+    }
+ 
+    cout << s << " " << d << endl;
+    
+
+    return 0;
+}
