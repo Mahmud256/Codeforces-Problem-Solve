040dac7ed787f5e94041de35dab8ff53a580b9d7
Mahmud Hasan <mahmud474849@gmail.com>
2024-01-21 22:38:18 +0600

    749A

---
+++ C++/749A - Bachgold Problem.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int a = n/2;
+    
+    cout<<a<<"\n";
+    
+    if(n%2==0)
+    {
+        while(a--)
+        {
+            cout<<2<<" ";
+        }
+    }
+    else{
+        a=a-1;
+        while(a--)
+        {
+            cout<<2<<" ";
+        }
+        cout<<2+1<<" ";
+    }
+
+    return 0;
+}
