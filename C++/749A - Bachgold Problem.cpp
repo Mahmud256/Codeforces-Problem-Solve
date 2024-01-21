60befe328f15ccc05a42e2c5eaed68682bb2e30c
Mahmud Hasan <mahmud474849@gmail.com>
2024-01-21 22:38:44 +0600

    749A

--- C++/749A - Bachgold Problem.cpp
+++ C++/749A - Bachgold Problem.cpp
@@ -5,26 +5,27 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    
-    int a = n/2;
-    
-    cout<<a<<"\n";
-    
-    if(n%2==0)
+    cin >> n;
+
+    int a = n / 2;
+
+    cout << a << "\n";
+
+    if (n % 2 == 0)
     {
-        while(a--)
+        while (a--)
         {
-            cout<<2<<" ";
+            cout << 2 << " ";
         }
     }
-    else{
-        a=a-1;
-        while(a--)
+    else
+    {
+        a = a - 1;
+        while (a--)
         {
-            cout<<2<<" ";
+            cout << 2 << " ";
         }
-        cout<<2+1<<" ";
+        cout << 2 + 1 << " ";
     }
 
     return 0;
